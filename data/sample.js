29eb56529601192d9d7c006a6a323c8575bdc99c
tinshade <tinshade98@gmail.com>
2021-11-05 22:31:36 +0530

    JSON response

---
+++ data/sample.js
@@ -0,0 +1,32 @@
+const products = [
+  { id: 1, name: "Product 1", price: 100 },
+  { id: 2, name: "Product 2", price: 200 },
+  { id: 3, name: "Product 3", price: 300 },
+  { id: 4, name: "Product 4", price: 400 },
+  { id: 5, name: "Product 5", price: 500 },
+];
+
+const people = [
+  {
+    id: 1,
+    name: "John Doe",
+    email: "johndoe@ymail.com",
+  },
+  {
+    id: 2,
+    name: "John Smith",
+    email: "johnsmith@ymail.com",
+  },
+  {
+    id: 3,
+    name: "Jane Doe",
+    email: "johndoe@ymail.com",
+  },
+  {
+    id: 4,
+    name: "Jane Smith",
+    email: "janesmith@ymail.com",
+  },
+];
+
+module.exports = { people, products };
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const { people, products } = require("./data/sample");
 const app = express();
 
 //!USE Request
@@ -22,6 +23,49 @@ app.get("/about", (req, res) => {
 
 app.get("/get_big_file", (req, res) => {
   res.sendFile(path.join(__dirname, "data/bigfile.txt"));
+});
+
+//! Getting all users
+app.get("/get_people", (req, res) => {
+  res.json(people);
+});
+
+//!Getting a single user
+app.get("/get_people/:userID", (req, res) => {
+  const userID = Number(req.params.userID);
+  const user = people.find((person) => person.id === userID);
+  if (!user) {
+    return res.status(404).send("User not found");
+  }
+  res.json(user);
+});
+
+app.get("/get_products", (req, res) => {
+  let curated = null;
+  if (req.query.name) {
+    curated = products.filter(
+      (product) => product.name.includes(req.query.name) //*Find by wildcard name
+    );
+  } else if (req.query.price) {
+    curated = products.filter(
+      (product) => product.price === req.query.price //*Find by exact price
+    );
+  }
+
+  if (!curated) {
+    return res.json(products); //!Getting filtered products
+  }
+  res.json(products); //!Getting all products
+});
+
+//!Getting a single product
+app.get("/get_products/:productID", (req, res) => {
+  const prodID = Number(req.params.userID);
+  const prod = products.find((product) => product.id === prodID);
+  if (!prod) {
+    return res.status(404).send("Product not found");
+  }
+  res.json(prod);
 });
 
 //!ALL Request
